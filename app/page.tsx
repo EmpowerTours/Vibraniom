1a78a9d55e31eac9f0fbd226ebafc39850db2d9a
EmpowerTours <162839159+EmpowerTours@users.noreply.github.com>
2025-08-24 02:40:48 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
-"use client";
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl mb-8">Welcome to Vibraniom</h1>
-      <p>Basic Next.js app is working!</p>
-    </main>
-  );
+  return <div>Welcome to Vibraniom</div>;
 }
